9ec517905f454be6c1d6aeb86d68b1f229d2b017
Namita Raghuvanshi <raghuvanshinamita@gmail.com>
2019-09-30 20:30:22 -0600

    receiving coordinates

--- src/Instruction/Land.java
+++ src/Instruction/Land.java
@@ -13,6 +13,16 @@ public class Land extends Instruction {
     public void executeInstruction(Communicator communicator, DroneState droneState) throws Exception {
         communicator.sendSignal("land");
         System.out.println(communicator.receiveSignal());
+        double yCoordinate;
+        double zCoordinate;
+        double xCoordinate;
+        yCoordinate=droneState.getPositionY();
+
+        zCoordinate=droneState.getPositionZ();
+        xCoordinate=droneState.getPositionX();
+        System.out.println("Coordinates before landing:"+"("+xCoordinate+","+yCoordinate+","+zCoordinate+")");
+        zCoordinate=0.00;
+        droneState.move(xCoordinate,yCoordinate,zCoordinate);
         Thread.sleep(2000);
     }
 
