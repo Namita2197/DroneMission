58c3dcc413ee2637917e3f192e4936952cb85a4a
Namita Raghuvanshi <raghuvanshinamita@gmail.com>
2019-09-30 23:06:37 -0600

    coordinates received

--- src/Instruction/TakeOff.java
+++ src/Instruction/TakeOff.java
@@ -14,6 +14,9 @@ public class TakeOff extends Instruction {
     public void executeInstruction(Communicator communicator, DroneState droneState) throws Exception {
         double zCoordinate;
         zCoordinate=droneState.getPositionZ();
+        double xCoordinate=droneState.getPositionX();
+        double ycoordinate=droneState.getPositionY();
+        xCoordinate=ycoordinate=zCoordinate=0.00;
         communicator.sendSignal("takeoff");
         zCoordinate=zCoordinate+70.00;
         droneState.move(0.00,0.00,zCoordinate);
