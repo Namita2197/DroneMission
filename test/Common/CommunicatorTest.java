2f8fa5ac54fd803628005f386365a7d1bc58c426
Namita Raghuvanshi <raghuvanshinamita@gmail.com>
2019-10-03 13:43:35 -0600

    communicator testing completed

--- test/Common/CommunicatorTest.java
+++ test/Common/CommunicatorTest.java
@@ -2,9 +2,9 @@ package Common;
 
 import org.junit.jupiter.api.Test;
 
-import java.net.BindException;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,33 +33,107 @@ class CommunicatorTest {
     }
 
     @Test
-    void testInitiation() throws Exception {
-//        Communicator communicator1=new Communicator("127.0.0.1",1234);
-//        Communicator communicator2=new Communicator(1234);
-//        communicator2.sendSignal("ok");
-//        communicator1.initiation();
-//        assertEquals("command", communicator2.receiveSignal());
-        // communicator2.sendSignal(");
-        //assertEquals("ok",communicator1.receiveSignal());
+    public void testCommunicatorAndDummyServer() throws InterruptedException {
+        Thread droneThread = new Thread(new CommandThread());
+        Thread serverThread = new Thread(new ServerThread());
+        droneThread.start();
+        Thread.sleep(3000);
+        serverThread.start();
     }
 
     @Test
-    void sendSignal() {
+    public void  testInitiation(){
+
     }
 
     @Test
-    void receiveSignal() {
+    public void testSetAddressandPortNumber() throws Exception {
+        Communicator communicator=new Communicator("127.0.0.1",8889);
+        InetAddress newAddress =InetAddress.getByName("127.0.0.2");
+        int newPort =3000;
+        communicator.setAddress(newAddress,newPort);
+        assertEquals(newAddress,communicator.getAddress());
+        assertEquals(newPort,communicator.getPortNumber());
     }
 
     @Test
-    void getAddress() {
+    public void testSendAndReceiveSignal() throws Exception {
+        Communicator sender =new Communicator("127.0.0.1",5000);
+        Communicator receiver =new Communicator(5000);
+        sender.sendSignal("checking");
+        assertEquals("checking",receiver.receiveSignal());
+        assertEquals(5000,sender.getPortNumber());
     }
 
-    @Test
-    void getPortNumber() {
+}
+
+class DummyServer{
+    public void run() throws Exception {
+        byte[] bytesReceived = new byte[64];
+        byte[] bytesToSend;
+        DatagramSocket server = new DatagramSocket(7000);
+        DatagramPacket incomingPacket = new DatagramPacket(bytesReceived, bytesReceived.length);
+        try {
+            assert server != null;
+            server.receive(incomingPacket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String message = new String(incomingPacket.getData(), 0, incomingPacket.getLength());
+
+        InetAddress ipAddress = incomingPacket.getAddress();
+        if (message.equals("command")){
+            System.out.println("Command instruction received.");
+            bytesToSend = "ok".getBytes(StandardCharsets.UTF_8);
+        }
+        else {
+            System.out.println("No command instruction received.");
+            bytesToSend = "error".getBytes(StandardCharsets.UTF_8);
+        }
+        DatagramPacket sendPacket = new DatagramPacket(bytesToSend, bytesToSend.length, ipAddress, incomingPacket.getPort());
+        try {
+            server.send(sendPacket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(" Ok/Error sent back");
+
     }
 
-    @Test
-    void setAddress() {
+}
+
+class ServerThread implements Runnable {
+    @Override
+    public void run() {
+        DummyServer server = new DummyServer();
+        try {
+            server.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class CommandThread implements Runnable {
+    @Override
+    public void run() {
+        String ipAddress = "127.0.0.1";
+        int port = 6000;
+
+        Communicator communicator = null;
+        try {
+            communicator = new Communicator(ipAddress, port);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            communicator.sendSignal("command");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
